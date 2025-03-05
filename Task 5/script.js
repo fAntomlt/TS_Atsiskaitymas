da3b0b8a0201ca4541dc663f136f7fe02f6fc214
fAntomlt <tsalechovas@gmail.com>
2025-03-05 09:24:51 +0200

    Task 5 Completed

--- Task 5/script.js
+++ Task 5/script.js
@@ -7,12 +7,14 @@ Pvz.:
   "scissors", "rock" --> "Player 2 won!"
   "paper", "paper" --> "Draw!"
 -------------------------------------------------------------------------- */
-
-const zaidimas = (player1: 'scissors' | 'paper' | 'rock', player2: 'scissors' | 'paper' | 'rock') => {
-  if(player1 === player2){
-    return 'Draw!'
-  }else if((player1 === 'scissors' && player2 === 'paper') || (player1 === 'paper' && player2 === 'rock') || (player1 === 'rock' && player2 === 'scissors')){
-    return 'Player 1 won!'
-  }else{
-    return 'Player 2 won!'}
+var zaidimas = function (player1, player2) {
+    if (player1 === player2) {
+        return 'Draw!';
+    }
+    else if ((player1 === 'scissors' && player2 === 'paper') || (player1 === 'paper' && player2 === 'rock') || (player1 === 'rock' && player2 === 'scissors')) {
+        return 'Player 1 won!';
+    }
+    else {
+        return 'Player 2 won!';
+    }
 };
