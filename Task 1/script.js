f7c7bf77b642dad6232ad8ec9d02cd16e42cc920
fAntomlt <tsalechovas@gmail.com>
2025-03-05 08:32:51 +0200

    Compiled

---
+++ Task 1/script.js
@@ -0,0 +1,25 @@
+/* ------------------------------ TASK 1 -----------------------------------
+Priskirkite tipus VISIEMS pateiktiems kintamiesiems, kad kodas veiktų teisingai.
+Kur komentare parašyta "error", ta eilutė po tipo priskyrimo kintamąjam turėtų mesti klaidą. Pasitikrinus užkomentuoti visą eilutę, kad leistų sukompiliuoti.
+-------------------------------------------------------------------------- */
+var one = 654;
+var two = true;
+var three = true;
+var four = true;
+four = '123';
+var five = [1, 2, 3];
+five = [21, 22, 23, 24];
+// five = ['a', 'b']; // error
+// five = [true, true, false]; // error
+var six = [1, 2, 3, 4];
+six = ['a', 'b', 'c', 'd'];
+// six = ['a',2,3,'d']; // error
+// six = [true, false]; // error
+var seven = [1, 2, 3, 4];
+seven = ['a', 'b', 'c', 'd'];
+seven = ['a', 2, 3, 'd'];
+// seven = [true, false]; // error
+var eight = ['hi', 1];
+eight = ['bye', 2];
+// eight = [1,2]; // error
+// eight = ['a','b','c']; // error
